5b07d8ca6e8d20d3dd17a09c6ff702f2fe7f2fc1
michelebro <mibrowder94@gmail.com>
2023-01-11 18:42:01 -0800

    wip: begin process of mvc org

---
+++ models/fruits.js
@@ -0,0 +1,20 @@
+const fruits = [
+    {
+        name: 'apple',
+        color: 'red',
+        readyToEat: false        
+    },
+    {
+        name: 'pear',
+        color: 'green',
+        readyToEat: true        
+    },
+    {
+        name: 'banana',
+        color: 'yellow',
+        readyToEat: true        
+    },
+];
+
+
+module.exports = fruits;
